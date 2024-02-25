1b1efcecd79e8e67d58b266518f92ba3ebf5c172
Angshu Das <angshudas191@gmail.com>
2024-02-25 21:38:43 +0530

    modified

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let currentSong = new Audio();
+
 async function getSong(){
     const dir = await fetch("http://127.0.0.1:5500/songs/");
     const response = await dir.text();
@@ -14,11 +16,29 @@ async function getSong(){
     return songs;
 }
 
-async function main(){
-    let songs = await getSong();
+function secondsToMinSec(seconds) {
+    const mins = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+    const formattedMins = String(mins).padStart(2, '0');
+    const formattedSecs = String(secs).padStart(2, '0');
+    return `${formattedMins}:${formattedSecs}`;
+  }
+  
 
+playMusic = (track)=>{
+    currentSong.src = `/songs/${track}`;
+    currentSong.play();
+    play.src = 'resources/pause.svg'
+    document.querySelector('.song-information').innerHTML = `${track}`;
+    document.querySelector('.song-time').innerHTML = "00.00/00.00";
+} 
+
+async function main(){
+
+
+    // get songs and apped them into the playlist.
+    let songs = await getSong();
     let songUl = document.querySelector('.song-list').getElementsByTagName('ul')[0];
-    console.log(songUl);
     for(const song of songs){
         songUl.innerHTML = songUl.innerHTML + `
                                         <li class="flex">
@@ -32,11 +52,33 @@ async function main(){
                                         </li>`
     }
     
-    let audio = new Audio(songs[0]);
-    // audio.play();
 
-    audio.addEventListener('loadeddata', function(){
-        console.log(audio.duration, audio.currentSrc, audio.currentTime)
+    //Select all songs from the list
+    Array.from(document.querySelector('.song-list').getElementsByTagName('li')).forEach(function(e){
+        e.addEventListener('click', function(){
+            playMusic(e.querySelector('.song-name').innerHTML.trim());
+            
+        })
+        console.log(e.querySelector('.song-name').innerHTML);
+    })
+
+
+    //Attach listeners to the previous, next and play buttons
+    play.addEventListener('click', ()=>{
+        if(currentSong.paused){
+            currentSong.play();
+            play.src = 'resources/pause.svg'
+        }
+        else{
+            currentSong.pause();
+            play.src = 'resources/play.svg'
+        }
+    })
+
+
+    //Listen for time update events
+    currentSong.addEventListener('timeupdate', function(){
+      document.querySelector('.song-time').innerHTML = `${secondsToMinSec(currentSong.currentTime)}/${secondsToMinSec(currentSong.duration)}`;
     })
 }
 
